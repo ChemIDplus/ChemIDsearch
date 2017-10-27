7801f2f3ba2a92ee5ae0f4df8adcd5b750067167
Michael Chambers <michael.chambers@altum.com>
2017-10-27 11:49:52 -0400

    Removed no longer used tr search component

--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { HistoryComponent } from './history.component';
-import { TrSearchEventComponent } from './tr-search-event/tr-search-event.component';
 
 import { SharedModule } from './../shared/shared.module';
 import { HistoryRoutingModule } from './history-routing.module';
 
 @NgModule({
 	declarations: [
-		HistoryComponent,
-		TrSearchEventComponent
+		HistoryComponent
 	],
 	imports: [
 		SharedModule,
