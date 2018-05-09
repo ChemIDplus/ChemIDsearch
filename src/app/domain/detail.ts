f70b1fda109fa9ca4df2aaa406be3d109db58a5c
Richa <richa.singhal@dsfederal.com>
2018-05-09 15:44:02 -0400

    Fixed Sources display join

--- src/app/domain/detail.ts
+++ src/app/domain/detail.ts
@@ -8,8 +8,17 @@ export interface DetailMinJSON extends ElementMinJSON {
 /** Immutable */
 export class Detail extends Element {
 
-	constructor(data :string, readonly sources ? :ReadonlyArray<string>){
+	// readonly sourcesDisplay :string; // Causes "Type 'Detail' cannot be converted to type 'T'." in server-json.ts
+		// The error apparently occurs when trying to use generics and inheritance with classes where one or more member properties are not in the constructor;
+		// get method() "properties" also cause the error.
+		// Even non getter methods cause the error. Not sure what is going on!
+
+	/**
+	 * @param sourcesDisplay Param ignored and member's value generated from sources param instead. Adding this member within the constructor to avoid the issue with generics and inheritance.
+	 */
+	constructor(data :string, readonly sources ? :ReadonlyArray<string>, readonly sourcesDisplay ? :string){
 		super(data);
+		this.sourcesDisplay = (sources ? sources.join(', ') : undefined);
 	}
 
 	serialize() :DetailMinJSON {
